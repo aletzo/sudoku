17c12f2721d3fa9ff52edd3f523a66d8f1fa5170
ale tzo <ajo@mydevnull.net>
2011-09-28 00:18:40 +0300

    added a first playable version

--- js/app.js
+++ js/app.js
@@ -1,47 +1,76 @@
 var _puzzles = [
-    [
-        [8, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 4, 0, 0, 6, 0, 2],
-        [0, 2, 1, 7, 0, 0, 0, 0, 0],
-        [0, 0, 0, 3, 0, 0, 0, 0, 0],
-        [0, 0, 6, 5, 0, 4, 0, 0, 0],
-        [9, 1, 3, 0, 0, 6, 0, 0, 7],
-        [2, 0, 0, 0, 0, 3, 5, 0, 0],
-        [0, 0, 4, 0, 9, 0, 8, 0, 6],
-        [0, 0, 0, 0, 0, 8, 0, 0, 0]
-    ],
-    [
-        [0, 0, 0, 0, 4, 0, 8, 0, 0],
-        [1, 0, 0, 0, 0, 0, 0, 2, 0],
-        [0, 8, 2, 0, 7, 0, 0, 0, 0],
-        [0, 1, 0, 8, 5, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 4, 0, 3, 6],
-        [0, 0, 0, 0, 0, 0, 2, 0, 0],
-        [0, 7, 0, 5, 2, 0, 3, 0, 0],
-        [5, 0, 0, 3, 0, 0, 4, 8, 0],
-        [0, 0, 9, 0, 0, 1, 0, 0, 5]
-    ]
+[
+[8, 0, 0,   0, 0, 0,   0, 0, 0],
+[0, 0, 0,   4, 0, 0,   6, 0, 2],
+[0, 2, 1,   7, 0, 0,   0, 0, 0],
+[0, 0, 0,   3, 0, 0,   0, 0, 0],
+[0, 0, 6,   5, 0, 4,   0, 0, 0],
+[9, 1, 3,   0, 0, 6,   0, 0, 7],
+[2, 0, 0,   0, 0, 3,   5, 0, 0],
+[0, 0, 4,   0, 9, 0,   8, 0, 6],
+[0, 0, 0,   0, 0, 8,   0, 0, 0]
+], [
+[0, 0, 0,   0, 4, 0,   8, 0, 0],
+[1, 0, 0,   0, 0, 0,   0, 2, 0],
+[0, 8, 2,   0, 7, 0,   0, 0, 0],
+[0, 1, 0,   8, 5, 0,   0, 0, 0],
+[0, 0, 0,   0, 0, 4,   0, 3, 6],
+[0, 0, 0,   0, 0, 0,   2, 0, 0],
+[0, 7, 0,   5, 2, 0,   3, 0, 0],
+[5, 0, 0,   3, 0, 0,   4, 8, 0],
+[0, 0, 9,   0, 0, 1,   0, 0, 5]
+], [
+[0, 0, 6,   0, 0, 3,   2, 0, 0],
+[0, 0, 0,   0, 8, 0,   0, 9, 0],
+[3, 0, 8,   0, 4, 0,   0, 0, 0],
+[0, 0, 0,   5, 0, 0,   0, 0, 0],
+[0, 1, 5,   0, 0, 2,   0, 0, 8],
+[0, 6, 7,   0, 0, 0,   1, 0, 3],
+[0, 0, 0,   0, 7, 0,   0, 0, 9],
+[1, 0, 0,   0, 0, 0,   4, 0, 0],
+[0, 7, 0,   0, 1, 4,   0, 0, 0]
+], [
+[0, 9, 0,   5, 0, 0,   0, 0, 0],
+[0, 0, 3,   7, 0, 0,   4, 0, 0],
+[0, 7, 0,   0, 2, 0,   8, 6, 0],
+[3, 0, 0,   0, 4, 0,   0, 0, 0],
+[8, 0, 0,   0, 0, 0,   1, 7, 0],
+[0, 0, 5,   0, 0, 0,   6, 0, 0],
+[0, 8, 1,   0, 0, 9,   0, 2, 0],
+[0, 5, 0,   0, 7, 6,   0, 0, 0],
+[0, 0, 2,   0, 0, 0,   9, 0, 0]
+]
 ];
 
+newPuzzle();
+
+calculateStats();
+
 $('#new').click(function() {
-    puzzle = _puzzles[Math.floor(Math.random()*_puzzles.length)];
-    
-    for (i = 0; i < 9; i++) {
-        x = i + 1;
-        for (j = 0; j < 9; j++) {
-            y = j + 1;
-            value = puzzle[i][j];
-            
-            $('#cell_' + x + '_' + y + ' input').val(value == 0 ? '' : value);
-        }
+    newPuzzle();
+});
+
+$('div.cell input').dblclick(function(e) {
+    if (!$(this).hasClass('readonly')) {
+        $(this).toggleClass('maybe');
+        
+        e.preventDefault();
     }
 });
 
 
 $(document).keydown(function(e){
-    var _selectedCell = $('div.cell input[type=text]:focus');
+    var _selectedCell = $('div.cell input:focus');
     
     if (_selectedCell.length) {
+        if (e.keyCode == 46 || e.keyCode == 8) {
+            if (_selectedCell.attr('readonly') !== 'readonly') {
+                _selectedCell.val('').removeClass('maybe');
+            }
+            
+            return;
+        }
+        
         var _selectedCellDivId = _selectedCell.parent('div').attr('id');
         
         var _x = getX(_selectedCellDivId);
@@ -86,25 +115,20 @@ $(document).keydown(function(e){
                 focusCell(other);
             }
         }
+        
+        if (!$(this).val().match(/[^\d]/)) {
+            $(this).val('');
+        }
     }
-    if (e.keyCode >36 && e.keyCode < 41) {
-        e.preventDefault();
-    }
+    
 });
 
-
-$('div.cell input').hoverIntent({
-    over: function() {
-        $(this).siblings('div').fadeIn(500);
-    },
-    out: function() {
-        $(this).siblings('div').fadeOut(300);
-    },
-    timeout: 500
-});
-
-$('div.color').click(function() {
-    $(this).siblings('input').addClass($(this).attr('rel'));
+$('div.cell input').blur(function() {
+    if (/^[0-9]+$/.test($(this).val()) === false) {
+        $(this).val('');
+    }
+    
+    calculateStats();
 });
 
 function getX(id) {
@@ -121,4 +145,44 @@ function getY(id) {
 
 function focusCell(cell) {
     cell.children('input').focus();
+}
+
+function newPuzzle() {
+    puzzle = _puzzles[Math.floor(Math.random()*_puzzles.length)];
+    
+    for (i = 0; i < 9; i++) {
+        x = i + 1;
+        for (j = 0; j < 9; j++) {
+            y = j + 1;
+            value = puzzle[i][j];
+            
+            if (value == 0) {
+                $('#cell_' + x + '_' + y + ' input').val('')
+                .attr('readonly', false)
+                .removeClass('readonly');
+            } else {
+                $('#cell_' + x + '_' + y + ' input').val(value)
+                .attr('readonly', true)
+                .addClass('readonly');
+            }
+        }
+    }   
+}
+
+function calculateStats() {
+    stats = [0, 0, 0,   0, 0, 0,   0, 0, 0];
+    
+    for (i = 0; i < 9; i++) {
+        x = i + 1;
+        for (j = 0; j < 9; j++) {
+            y = j + 1;
+                
+            stats[$('#cell_' + x + '_' + y + ' input').val() - 1]++;
+        }
+    }
+    
+    for (k = 0; k < 9; k++) {
+        stat = k + 1;
+        $('#stats_' + stat + ' span').html(stats[k]);
+    }
 }
